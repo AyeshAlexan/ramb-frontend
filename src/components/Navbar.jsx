2cffca0380217bbf47a5d018588393874eea0acb
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2026-02-05 13:32:37 +0530

    Finish the The Nav Bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import {motion, scale} from "framer-motion";
+import logo from "../assets/logo.jpeg";
+
+export default function Navbar(){
+    return(
+        <header className="topbar">
+            <div className="container topbar__inner">
+                <div className="brand">
+                    <img className="brand_logo" src={logo} alt="RAMB"/>
+                    <span className="brand__name">RAMB</span>
+                </div>
+
+                <div className="topbar__right">
+                    <div className="phone">
+                        <span className="phone__icon">📞</span>
+                        <span className="phone__text">021-222-4220</span>
+                    </div>
+                    
+                    <motion.a
+                       whiteHover={{scale: 1.05}}
+                       whiteTap={{scale: 0.98}}
+                       className="btn btn--gold"
+                       href="#apply"
+                    >
+                        Apply Now 
+                    </motion.a>
+                </div>
+            </div>
+
+            <nav className="nav">
+                <div className="container nav__inner">
+                    <a className="nav__link active" href="#home">Home</a>
+                    <a className="nav__link" href="#loans">Loans</a>
+                    <a className="nav__link" href="#travel">Travel & Transport</a>
+                    <a className="nav__link" href="#jobbank">JobBank</a>
+                    <a className="nav__link" href="#contact">Contact</a>
+                </div>
+            </nav>
+        </header>
+    );
+}
