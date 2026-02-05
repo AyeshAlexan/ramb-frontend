fe01e884f37523ed64c6ccd3494b263d2dd59eea
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2026-02-05 12:46:32 +0530

    Setup the App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Services from "./components/Services";
+import CallRamb from "./components/CallRamb";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Highlights />
+        <Services />
+        <CallRamb />
+      </main>
+      <Footer />
+    </>
   );
 }
 
-export default App;
