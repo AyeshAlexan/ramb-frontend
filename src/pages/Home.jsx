1e7cfd143db83eacfcfd80856e33051255acc920
Ayesh Alexan Anthonythasan <ayeshanthoythasan@gmail.com>
2026-02-05 15:49:03 +0530

    Adding the Pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import Highlights from "../components/Highlights";
+import Services from "../components/Services";
+import CallRamb from "../components/CallRamb";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Highlights />
+      <Services />
+      <CallRamb />
+    </>
+  );
+}
